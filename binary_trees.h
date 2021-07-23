4bc2352a4758a3756fb8246c12dbe56e94abba22
Olly Imanishimwe <imanishimweolyy@gmail.com>
2021-07-24 01:01:31 +0200

    binary_trees header edit

--- binary_trees.h
+++ binary_trees.h
@@ -68,4 +68,10 @@ heap_t *array_to_heap(int *array, size_t size);
 int heap_extract(heap_t **root);
 int *heap_to_sorted_array(heap_t *heap, size_t *size);
 
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
+int binary_tree_is_complete(const binary_tree_t *tree);
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
+binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
+
 #endif /* _BINARY_TREES_H_ */
